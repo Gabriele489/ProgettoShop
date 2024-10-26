b9aed4dfb9f221d7c71e97aa6b6e8472238d8406
Gabriele489 <127138366+Gabriele489@users.noreply.github.com>
2024-10-26 10:57:18 +0200

    Add files via upload

---
+++ code/Order/src/main/java/com/microservices/order/services/interfaces/OrderService.java
@@ -0,0 +1,18 @@
+package com.microservices.order.services.interfaces;
+
+import com.microservices.order.models.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderService {
+
+    //List user purchases : /purchases/{userId}
+    List<Order> getUserPurchases(String userId);
+
+    //Get purchase: /purchases/{userId}/{id}
+    Optional<Order> getPurchase(String userId, String id);
+
+    //Buy: POST /purchases
+    Optional<Order> buy(String userId, Integer count, String productId);
+}
